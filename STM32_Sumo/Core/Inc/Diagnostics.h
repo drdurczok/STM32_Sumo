693ece3abe03ba4d14328e12a25ffff2971363ec
DamianDurczok <drdurczok@gmail.com>
2020-12-30 21:28:35 +0100

    File Structure Update

---
+++ STM32_Sumo/Core/Inc/Diagnostics.h
@@ -0,0 +1,19 @@
+/*
+ * Diagnostics.h
+ *
+ *  Created on: Dec 30, 2020
+ *      Author: Damian
+ */
+
+#ifndef SRC_DIAGNOSTICS_H_
+#define SRC_DIAGNOSTICS_H_
+
+class Diagnostics {
+public:
+	Diagnostics();
+	virtual ~Diagnostics();
+
+	void Diagnostics::check_UART2();
+};
+
+#endif /* SRC_DIAGNOSTICS_H_ */
---
+++ STM32_Sumo/Core/Src/Diagnostics.cpp
@@ -0,0 +1,21 @@
+/*
+ * Diagnostics.cpp
+ *
+ *  Created on: Dec 30, 2020
+ *      Author: Damian
+ */
+
+#include "Diagnostics.h"
+
+Diagnostics::Diagnostics() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Diagnostics::~Diagnostics() {
+	// TODO Auto-generated destructor stub
+}
+
+void Diagnostics::check_UART2(){
+
+}
